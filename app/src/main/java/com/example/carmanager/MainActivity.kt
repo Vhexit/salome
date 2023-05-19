c5b28dee2bbbbb7af3dc6bad60453691f3ad1dd8
Victor <vhexit@gmail.com>
2023-05-19 16:35:47 +0300

    salome

---
+++ app/src/main/java/com/example/carmanager/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.carmanager
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var btnLogin: Button
+    private lateinit var createAcc: Button
+    private lateinit var emailedt: EditText
+    private lateinit var passwordedt: EditText
+
+    private lateinit var inputphone: EditText
+
+    //initialize firebase
+    private lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //find our views
+        emailedt = findViewById(R.id.edtEmail)
+        passwordedt = findViewById(R.id.edtPassword)
+        btnLogin = findViewById(R.id.login_btn)
+        createAcc = findViewById(R.id.acc_btn)
+
+        //initialize firebase
+        auth = Firebase.auth
+
+
+        createAcc.setOnClickListener {
+
+            var intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+        btnLogin.setOnClickListener {
+
+            loginUser()
+
+        }
+
+
+
+
+
+    }
+
+    private fun loginUser() {
+        //Toast.makeText(this, "Trying to Login", Toast.LENGTH_SHORT).show()
+        //Convert inputs to string
+        val userEmail = emailedt.text.toString()
+        val userPassword = passwordedt.text.toString()
+
+        //Toast.makeText(this, "email is $userEmail and password is $userPassword", Toast.LENGTH_LONG).show()
+
+        auth.signInWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener {
+            if (it.isSuccessful) {
+                //means if task is successful navigate to landing page
+                var intent = Intent(this, Landingpage::class.java)
+                startActivity(intent)
+
+
+            }
+            else {
+                Toast.makeText(this, "Failed to Sign in", Toast.LENGTH_LONG).show()
+
+                Log.d("TAG", "Error--->", it.exception)
+            }
+        }
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/carmanager/RegisterActivity.kt
@@ -0,0 +1,89 @@
+package com.example.carmanager
+
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class RegisterActivity : AppCompatActivity() {
+
+    lateinit var Buttonreg:Button
+    lateinit var emailinp:EditText
+    lateinit var passinp:EditText
+    lateinit var confpassinp:EditText
+
+    private lateinit var auth: FirebaseAuth
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        //find our ids
+        Buttonreg=findViewById(R.id.login_btn)
+        emailinp = findViewById(R.id.edtEmail)
+        passinp = findViewById(R.id.edtPassword)
+        confpassinp =findViewById(R.id.edtConfPassword)
+
+        //initialize firebase Auth
+        auth = Firebase.auth
+
+
+
+
+        Buttonreg.setOnClickListener {
+
+            //user sign up method
+            signUpUser()
+
+        }
+
+
+    }
+
+    private fun signUpUser() {
+
+        val userEmail = emailinp.text.toString()
+        val userPass = passinp.text.toString()
+        val confirmPass = confpassinp.text.toString()
+
+        //perform input validation if blank
+        if (userEmail.isBlank() || userPass.isBlank() || confirmPass.isBlank()) {
+            Toast.makeText(this, "One of your inputs is empty", Toast.LENGTH_SHORT).show()
+//chai , Juice , uji
+        }
+        //check if password is matching
+        if (userPass != confirmPass) {
+            Toast.makeText(this, "Sorry Password is not matching", Toast.LENGTH_SHORT).show()
+        }
+        //create user with firebase auth
+        auth.createUserWithEmailAndPassword(userEmail, userPass).addOnCompleteListener(this) {
+            if (it.isSuccessful ) {
+                Toast.makeText(this, "Successfully Signed Up", Toast.LENGTH_LONG).show()
+                finish()
+                //Navigate to another page
+                var intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+
+            }
+            else {
+                Toast.makeText(this, "Failed to Sign Up", Toast.LENGTH_SHORT).show()
+                Log.d("TAG", "Error--->", it.exception)
+            }
+        }
+
+
+
+
+
+    }
+}
